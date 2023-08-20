0302259fe113a00dd825531781e944164b4b6bd4
emon1432 <ovirajemon143@gmail.com>
2023-08-20 11:30:21 +0600

    Start doctorappointment System

---
+++ controllers/userController.js
@@ -0,0 +1,118 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Doctor = require("../models/doctorModel");
+const Appointment = require("../models/appointmentModel");
+
+const getuser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    return res.send(user);
+  } catch (error) {
+    res.status(500).send("Unable to get user");
+  }
+};
+
+const getallusers = async (req, res) => {
+  try {
+    const users = await User.find()
+      // .find({ _id: { $ne: req.locals } })
+      .select("-password");
+      console.log("🚀 ~ file: userController.js:21 ~ getallusers ~ users:", req.locals)
+      
+    return res.send(users);
+  } catch (error) {
+    res.status(500).send("Unable to get all users");
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const emailPresent = await User.findOne({ email: req.body.email });
+    if (!emailPresent) {
+      return res.status(400).send("Incorrect credentials");
+    }
+    const verifyPass = await bcrypt.compare(
+      req.body.password,
+      emailPresent.password
+    );
+    if (!verifyPass) {
+      return res.status(400).send("Incorrect credentials");
+    }
+    const token = jwt.sign(
+      { userId: emailPresent._id, isAdmin: emailPresent.isAdmin },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2 days",
+      }
+    );
+    return res.status(201).send({ msg: "User logged in successfully", token });
+  } catch (error) {
+    res.status(500).send("Unable to login user");
+  }
+};
+
+const register = async (req, res) => {
+  console.log("🚀 ~ file: userController.js:54 ~ register ~ req:", req.body)
+  try {
+    const emailPresent = await User.findOne({ email: req.body.email });
+    console.log("🚀 ~ file: userController.js:56 ~ register ~ emailPresent:", emailPresent)
+    if (emailPresent) {
+      return res.status(400).send("Email already exists");
+    }
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    // const user = new User({ ...req.body, password: hashedPass });
+    
+    User.create({ ...req.body, password: hashedPass });
+    console.log('here');
+    // console.log(result);
+    // if (!result) {
+    //   return res.status(500).send("Unable to register user");
+    // }
+    return res.status(201).send("User registered successfully");
+  } catch (error) {
+    console.log(error);
+    res.send(error.message)
+    // res.status(500).send("Unable to register user",error);
+  }
+};
+
+const updateprofile = async (req, res) => {
+  try {
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const result = await User.findByIdAndUpdate(
+      { _id: req.locals },
+      { ...req.body, password: hashedPass }
+    );
+    if (!result) {
+      return res.status(500).send("Unable to update user");
+    }
+    return res.status(201).send("User updated successfully");
+  } catch (error) {
+    res.status(500).send("Unable to update user");
+  }
+};
+
+const deleteuser = async (req, res) => {
+  try {
+    const result = await User.findByIdAndDelete(req.body.userId);
+    const removeDoc = await Doctor.findOneAndDelete({
+      userId: req.body.userId,
+    });
+    const removeAppoint = await Appointment.findOneAndDelete({
+      userId: req.body.userId,
+    });
+    return res.send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send("Unable to delete user");
+  }
+};
+
+module.exports = {
+  getuser,
+  getallusers,
+  login,
+  register,
+  updateprofile,
+  deleteuser,
+};
